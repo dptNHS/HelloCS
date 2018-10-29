1bf599497e5d9a5a55d83976d6613021dae57d9a
Nick Ajderian <nick@flikk.net>
2018-10-29 14:01:59 +0000

    Prompts for name

--- HelloCS/Program.cs
+++ HelloCS/Program.cs
@@ -10,8 +10,25 @@ namespace HelloCS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello CS");
-            var s = Console.ReadLine();
+            string userName = null;
+            while (true)
+            {
+                Console.WriteLine("Hello, what is your name?");
+                userName = Console.ReadLine();
+                if (!string.IsNullOrEmpty(userName))
+                {
+                    break;
+                }
+            }
+            var greeting = "Good Morning";
+            if(DateTime.Now.Hour >= 12)
+            {
+                greeting = "Good Afternoon";
+            }
+            Console.WriteLine(
+                string.Format("{0} {1}", greeting, userName));
+
+            Console.ReadLine();
         }
     }
 }
